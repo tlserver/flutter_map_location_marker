9fa292d3aa64b585b39e34410b58c2395c98925b
Thomas Coumau <thomasco@bam.tech>
2023-05-17 16:55:13 +0200

    remove dependancies

--- lib/flutter_map_location_marker.dart
+++ lib/flutter_map_location_marker.dart
@@ -2,9 +2,7 @@
 library flutter_map_location_marker;
 
 export 'src/animated_location_marker_layer.dart';
-export 'src/current_location_layer.dart';
 export 'src/data.dart';
-export 'src/data_stream_factory.dart';
 export 'src/drawing/default_location_marker.dart';
 export 'src/drawing/heading_sector.dart';
 export 'src/follow_on_location_update.dart';
