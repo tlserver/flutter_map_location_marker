c834beeb495b1b4ae9f686e5a078ea97ae1d14c0
6y <tlserver6y@gmail.com>
2023-09-13 09:46:33 +0800

    upgrade to flutter_compass v0.8.0

--- lib/src/data_stream_factory.dart
+++ lib/src/data_stream_factory.dart
@@ -176,6 +176,6 @@ class LocationMarkerDataStreamFactory {
   /// Create a heading stream which is used as default value of
   /// [CurrentLocationLayer.headingStream].
   Stream<CompassEvent?> defaultHeadingStreamSource() {
-    return !kIsWeb ? FlutterCompass.events! : const Stream.empty();
+    return FlutterCompass.events!;
   }
 }
