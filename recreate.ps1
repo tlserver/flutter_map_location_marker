d1f4aadb70a0ace5a02a9ba3739048b89e03e289
6y <tlserver6y@gmail.com>
2026-06-11 13:43:02 +0800

    add recreate flutter project script

---
+++ recreate.ps1
@@ -0,0 +1,20 @@
+flutter create --no-pub --overwrite . `
+               --project-name "flutter_map_location_marker" `
+               --description "A flutter map plugin for displaying device current location." `
+               --org "net.tlserver6y" `
+               --template "package"
+
+flutter create --no-pub --overwrite --empty example `
+               --project-name "flutter_map_location_marker_example" `
+               --description "Example of FlutterMapLocationMarkerPlugin" `
+               --org "net.tlserver6y"
+
+git checkout HEAD -- ".\.gitignore"
+git checkout HEAD -- ".\CHANGELOG.md"
+Remove-Item -Path    ".\example\README.md"
+git checkout HEAD -- ".\example\lib\*"
+git checkout HEAD -- ".\flutter_map_location_marker.iml"
+git checkout HEAD -- ".\LICENSE"
+git checkout HEAD -- ".\lib\*"
+git checkout HEAD -- ".\README.md"
+Remove-Item -Path    ".\test" -Recurse
